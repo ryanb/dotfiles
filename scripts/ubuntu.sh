8a48b3db4997b32619834b3f88c06e44eb21442e
johannes-kostas goetzinger <dbldots@gmail.com>
2015-10-26 11:42:20 +0100

    installs neovim & chruby on ubuntu

--- scripts/ubuntu.sh
+++ scripts/ubuntu.sh
@@ -4,6 +4,15 @@ sudo apt-get install -y curl git ctags zsh autojump htop
 sudo apt-get install -y automake pkg-config libpcre3-dev zlib1g-dev liblzma-dev
 sudo apt-get install -y golang
 
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt-get update
+sudo apt-get install neovim
+
+wget -O chruby-0.3.9.tar.gz https://github.com/postmodern/chruby/archive/v0.3.9.tar.gz
+tar -xzvf chruby-0.3.9.tar.gz
+cd chruby-0.3.9/
+sudo make install
+
 # neovim
 if [ ! -d "$HOME/the_silver_searcher" ]; then
   cd
