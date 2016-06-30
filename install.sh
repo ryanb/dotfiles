38e3f71b01dab212c115643ec750fbbd229d7e55
Pete Yandell <pete@notahat.com>
2016-07-01 00:56:06 +1000

    Make Ruby install also install bundler correctly.

--- install.sh
+++ install.sh
@@ -55,6 +55,7 @@ ruby_version=`cat ~/.ruby-version`
 if [ ! -d ~/.rubies/ruby-$ruby_version ]; then
   ruby-install ruby $ruby_version
   source /usr/local/opt/chruby/share/chruby/chruby.sh
+  chruby $ruby_version
   gem install bundler
 else
   echo "Ruby $ruby_version is already installed, skipping."
