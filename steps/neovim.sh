57db37a4a15922c730c5fdea532d56ab9dc5b507
Pete Yandell <pete@notahat.com>
2016-09-24 12:09:41 +1000

    Make neovim install remind you to do a PlugInstall

--- steps/neovim.sh
+++ steps/neovim.sh
@@ -5,3 +5,6 @@ link_file ~/.dotfiles/nvim/vimplug.vim ~/.config/nvim/vimplug.vim
 if [ ! -f ~/.config/nvim/autoload/plug.vim ]; then
   curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
+
+echo
+echo "You'll need to do a manual :PlugInstall to finish the job."
