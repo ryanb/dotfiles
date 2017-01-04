54750b089c39f839f6ecffd641230f9dba6e99d2
johannes-kostas goetzinger <dbldots@gmail.com>
2017-01-04 17:14:05 +0100

    changes tmux script

--- scripts/tmux.sh
+++ scripts/tmux.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 wget https://raw.githubusercontent.com/dbldots/dotfiles/master/files/_tmux.conf -O ~/.johannes.tmux.conf
-tmux -f ~/.johannes.tmux.conf
+tmux
+tmux source-file ~/.johannes.tmux.conf
