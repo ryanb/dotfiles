41d510901e56a664b948d3425e308b8d41f65be3
johannes-kostas goetzinger <dbldots@gmail.com>
2017-01-18 12:10:26 +0100

    updates tmux script

--- scripts/tmux.sh
+++ scripts/tmux.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 wget https://raw.githubusercontent.com/dbldots/dotfiles/master/files/_tmux.conf -O ~/.johannes.tmux.conf
-tmux
-tmux source-file ~/.johannes.tmux.conf
+tmux new-session -d -s johannes
+tmux send-keys -t johannes 'tmux source-file ~/.johannes.tmux.conf' C-j
+tmux attach -t johannes
