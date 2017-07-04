b3e6d844972e485c81de95b1e19386577e34e7f8
Pete Yandell <pete@notahat.com>
2017-07-04 14:34:50 +1000

    Install yarn with homebrew rather than npm
    That's what the yarn folks recommend nowadays.

--- steps/node.sh
+++ steps/node.sh
@@ -3,11 +3,18 @@ link_config_files node-version
 node_version=`cat ~/.node-version`
 if [ ! -d ~/.nodenv/versions/$node_version ]; then
   nodenv install $node_version
+
   npm install npm -g
   npm install standard -g
   npm install babel-eslint -g
-  npm install yarn -g
   nodenv rehash
+
+  # I do this here rather than in the Brewfile, coz we need node installed
+  # first, and I can't figure out how to get the --ignore-dependencies to work
+  # in the Brewfile.
+  if [ ! -f /usr/local/bin/yarn ]; then
+    brew install yarn --ignore-dependencies
+  fi
 else
   echo "Node $node_version is already installed, skipping."
 fi
