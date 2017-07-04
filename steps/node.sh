b45ac658f2a784038e8de596d317d0fbbf5690eb
Pete Yandell <pete@notahat.com>
2017-07-04 16:27:09 +1000

    Prefer installing things with yarn rather than npm.

--- steps/node.sh
+++ steps/node.sh
@@ -4,10 +4,8 @@ node_version=`cat ~/.node-version`
 if [ ! -d ~/.nodenv/versions/$node_version ]; then
   nodenv install $node_version
 
+  # Get the current version of npm.
   npm install npm -g
-  npm install standard -g
-  npm install babel-eslint -g
-  nodenv rehash
 
   # I do this here rather than in the Brewfile, coz we need node installed
   # first, and I can't figure out how to get the --ignore-dependencies to work
@@ -15,6 +13,11 @@ if [ ! -d ~/.nodenv/versions/$node_version ]; then
   if [ ! -f /usr/local/bin/yarn ]; then
     brew install yarn --ignore-dependencies
   fi
+
+  yarn global add standard
+  yarn global add create-react-app
+
+  nodenv rehash
 else
   echo "Node $node_version is already installed, skipping."
 fi
