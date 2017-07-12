a455d82bd48558975963d156c7f7d22b80ecbbfa
Pete Yandell <pete@notahat.com>
2017-07-13 00:02:25 +1000

    Bump node to 6.11.1

--- steps/node.sh
+++ steps/node.sh
@@ -3,6 +3,7 @@ link_config_files node-version
 node_version=`cat ~/.node-version`
 if [ ! -d ~/.nodenv/versions/$node_version ]; then
   nodenv install $node_version
+  nodenv rehash
 
   # Get the current version of npm.
   npm install npm -g
@@ -16,7 +17,6 @@ if [ ! -d ~/.nodenv/versions/$node_version ]; then
 
   yarn global add standard
   yarn global add create-react-app
-
   nodenv rehash
 else
   echo "Node $node_version is already installed, skipping."
