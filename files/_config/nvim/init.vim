b95302d67067d8fee31f8f0781a8e3a6a4a70798
johannes-kostas goetzinger <dbldots@gmail.com>
2018-01-12 18:14:08 +0100

    updates scripts to install ripgrep, neovim on linux and

--- files/_config/nvim/init.vim
+++ files/_config/nvim/init.vim
@@ -92,6 +92,9 @@ let g:airline_powerline_fonts=1
 if executable('ag')
   let g:ackprg = 'ag --vimgrep'
 endif
+if executable('rg')
+  let g:ackprg = 'rg --vimgrep'
+endif
 
 " folding
 augroup vimrc
--- scripts/ubuntu.sh
+++ scripts/ubuntu.sh
@@ -8,6 +8,7 @@ sudo apt-get update
 
 sudo apt-get install -y automake pkg-config libpcre3-dev zlib1g-dev liblzma-dev software-properties-common
 sudo apt-get install -y curl git ctags zsh autojump htop
+sudo apt-get -y install python3-pip
 
 sudo apt-get install -y neovim
 sudo apt-get install -y tmate
@@ -20,13 +21,22 @@ go get github.com/dbldots/goclip
 sudo ln -s $GOPATH/bin/goclip /usr/local/bin/xsel
 
 # neovim
-if [ ! -d "$HOME/the_silver_searcher" ]; then
-  cd
-  git clone https://github.com/ggreer/the_silver_searcher.git
-  cd the_silver_searcher
-  ./build.sh
-  sudo make install
-fi
+# if [ ! -d "$HOME/the_silver_searcher" ]; then
+#   cd
+#   git clone https://github.com/ggreer/the_silver_searcher.git
+#   cd the_silver_searcher
+#   ./build.sh
+#   sudo make install
+# fi
+
+# ripgrep
+cd ~
+wget https://github.com/BurntSushi/ripgrep/releases/download/0.7.1/ripgrep-0.7.1-x86_64-unknown-linux-musl.tar.gz
+tar xvfz ripgrep-0.7.1-x86_64-unknown-linux-musl.tar.gz
+sudo cp ripgrep-0.7.1-x86_64-unknown-linux-musl/rg /usr/local/bin
+sudo chmod +x /usr/local/bin/rg
+
+sudo pip3 install neovim
 
 # set shell to zsh
 sudo chsh -s `which zsh` $USER
