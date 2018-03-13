1bb8ce7a13510bbebee14902d652361c28cb22b4
Pete Yandell <pete@notahat.com>
2018-03-14 10:21:43 +1100

    Bump node version to 8.10.0, and tweak how I install things.

--- steps/node.sh
+++ steps/node.sh
@@ -8,12 +8,8 @@ if [ ! -d ~/.nodenv/versions/$node_version ]; then
   # Get the current version of npm.
   npm install npm -g
 
-  # I do this here rather than in the Brewfile, coz we need node installed
-  # first, and I can't figure out how to get the --ignore-dependencies to work
-  # in the Brewfile.
-  if [ ! -f /usr/local/bin/yarn ]; then
-    brew install yarn --ignore-dependencies
-  fi
+  # Grab yarn, coz it's better.
+  npm install yarn -g
 
   # I'd love to use yarn for these, but yarn global installs don't play nice
   # with nodenv.
