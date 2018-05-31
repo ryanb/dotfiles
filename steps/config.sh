05438014b20d4f651f125f6d036714886d8aedcc
Pete Yandell <pete@notahat.com>
2018-05-31 10:04:14 +1000

    Move dock to the right

--- steps/config.sh
+++ steps/config.sh
@@ -6,7 +6,7 @@ defaults write com.apple.dashboard mcx-disabled -boolean YES
 # Clear out the dock.
 defaults write com.apple.dock checked-for-launchpad -boolean YES
 defaults write com.apple.dock persistent-apps "()"
-defaults write com.apple.dock orientation left
+defaults write com.apple.dock orientation right
 killall Dock
 
 # Set up menu bar extras.
