f9dc77c598b527603a5af724ca600acc02402941
Pete Yandell <pete@notahat.com>
2018-11-07 20:36:12 +1100

    Put iTerm2 config into here

---
+++ steps/iterm2.sh
@@ -0,0 +1,5 @@
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.dotfiles"
+
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+
+echo Installed.
