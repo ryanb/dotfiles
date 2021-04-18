6d8793e7e0d4d51e83e53f3448b535b4f4e66a38
Pete Yandell <pete@notahat.com>
2021-04-18 22:18:14 +1000

    Fix broken command in config.sh

--- steps/config.sh
+++ steps/config.sh
@@ -2,5 +2,5 @@ link_config_files gitignore zshrc
 
 # Clear out the dock.
 defaults write com.apple.dock persistent-apps "()"
-defaults write com.apple.dock show-recents --boolean NO
+defaults write com.apple.dock show-recents -boolean FALSE
 killall Dock
