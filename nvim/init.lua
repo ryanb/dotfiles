4e842b55b5610aadb2ea04c630857bfcadec58ac
Pete Yandell <pete@notahat.com>
2021-10-31 12:46:00 +1100

    Much neovim config tinkering

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,30 +2,93 @@
 -- Packages
 --
 local packer = require 'packer'
-packer.startup(function()
+packer.startup(function(use)
+  -- Use packer to manage itself
   use 'wbthomason/packer.nvim'
 
+  -- Syntax highlighting
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
 
+  -- Completion
   use 'hrsh7th/nvim-cmp'
   use 'hrsh7th/cmp-nvim-lsp'
 
+  -- Language server
   use 'neovim/nvim-lspconfig'
 
+  -- Fancy fuzzy finder
   use 'nvim-lua/plenary.nvim'
   use 'nvim-telescope/telescope.nvim'
 
+  -- File navigation
+  use 'kyazdani42/nvim-tree.lua'
+
+  -- Git diffs in the sign column
+  use 'lewis6991/gitsigns.nvim'
+
+  -- Commenting
+  use 'tpope/vim-commentary'
+
+  -- Color scheme
   use 'rktjmp/lush.nvim'
   use 'metalelf0/jellybeans-nvim'
 
+  -- Fancy icons (for telescope, nvim-tree, and lualine)
   use 'kyazdani42/nvim-web-devicons'
+
+  -- Fancy status line
+  use 'nvim-lualine/lualine.nvim'
 end)
+
+
+----------------------------------------------------------------------
+-- General options
+--
+local opt = vim.opt
+local cmd = vim.cmd
+
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.softtabstop = 2
+opt.expandtab = true
+opt.breakindent = true
+opt.linebreak = true
+
+opt.relativenumber = true
+
+opt.mouse = 'a'
+
+opt.completeopt = { 'menu', 'menuone', 'noselect' }
+
+
+----------------------------------------------------------------------
+-- Key bindings
+--
+vim.g.mapleader = ','
+
+local set_keymap = vim.api.nvim_set_keymap
+local opts = { noremap = true }
+
+set_keymap('n', '<leader>f', '<cmd>Telescope find_files<CR>', opts)
+set_keymap('n', '<leader>b', '<cmd>Telescope buffers<CR>', opts)
+set_keymap('n', '<leader>a', '<cmd>Telescope lsp_code_actions<CR>', opts)
+set_keymap('n', '<leader>t', '<cmd>NvimTreeToggle<CR>', opts)
+
+-- Reselect the visual area when changing indenting in visual mode.
+set_keymap('v', '<', '<gv', opts)
+set_keymap('v', '>', '>gv', opts)
+
+-- Shortcuts for navigation between windows
+set_keymap('n', '<c-h>', '<c-w>h', opts)
+set_keymap('n', '<c-j>', '<c-w>j', opts)
+set_keymap('n', '<c-k>', '<c-w>k', opts)
+set_keymap('n', '<c-l>', '<c-w>l', opts)
 
 
 ----------------------------------------------------------------------
 -- Syntax highlighting
 --
-local treesitter = require('nvim-treesitter.configs')
+local treesitter = require 'nvim-treesitter.configs'
 treesitter.setup({
   ensure_installed = { 'lua', 'javascript', 'typescript', 'tsx', 'css', 'scss', 'ruby' },
   highlight = { enable = true }
@@ -42,70 +105,66 @@ cmp.setup({
   })
 })
 
-local cmp_nvim_lsp = require 'cmp_nvim_lsp'
-local client_capabilities = vim.lsp.protocol.make_client_capabilities()
-local cmp_capabilities = cmp_nvim_lsp.update_capabilities(client_capabilities)
-
 
 ----------------------------------------------------------------------
 -- Language server
 --
 local lsp = require 'lspconfig'
+local cmp_nvim_lsp = require 'cmp_nvim_lsp'
 
+-- This gets run when LSP attaches to a buffer.
 local on_attach = function(client, buffer_number)
-  local nvim_buf_set_keymap = vim.api.nvim_buf_set_keymap
+  local buf_set_keymap = vim.api.nvim_buf_set_keymap
   local opts = { noremap = true, silent = true }
-  nvim_buf_set_keymap(buffer_number, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  nvim_buf_set_keymap(buffer_number, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  buf_set_keymap(buffer_number, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap(buffer_number, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  buf_set_keymap(buffer_number, 'n', 'K', "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
 end
 
+local client_capabilities = vim.lsp.protocol.make_client_capabilities()
+local cmp_capabilities = cmp_nvim_lsp.update_capabilities(client_capabilities)
+
 lsp.tsserver.setup({
-  capabilities = cmp_capabilities,
-  on_attach = on_attach
+  on_attach = on_attach,
+  capabilities = cmp_capabilities
 })
 
 lsp.solargraph.setup({
-  capabilities = cmp_capabilities,
-  on_attach = on_attach
+  on_attach = on_attach,
+  capabilities = cmp_capabilities
 })
 
 
 ----------------------------------------------------------------------
--- Dev icons to make Telescope better
+-- File navigation
 --
-require('nvim-web-devicons').setup({ default = true })
+local nvim_tree = require 'nvim-tree'
+nvim_tree.setup({ filters = { dotfiles = true } })
 
 
 ----------------------------------------------------------------------
--- Key bindings
+-- Git diffs in the sign column
 --
-vim.g.mapleader = ','
-
-local nvim_set_keymap = vim.api.nvim_set_keymap
-local opts = { noremap = true }
-
-nvim_set_keymap('n', '<leader>f', '<cmd>Telescope find_files<CR>', opts)
-nvim_set_keymap('n', '<leader>b', '<cmd>Telescope buffers<CR>', opts)
-nvim_set_keymap('n', '<leader>a', '<cmd>Telescope lsp_code_actions<CR>', opts)
+local gitsigns = require 'gitsigns'
+gitsigns.setup()
 
 
 ----------------------------------------------------------------------
--- General options
+-- Colors
 --
-local opt = vim.opt
-local cmd = vim.cmd
-
 opt.termguicolors = true
 cmd 'colorscheme jellybeans-nvim'
 
-opt.tabstop = 2
-opt.shiftwidth = 2
-opt.softtabstop = 2
-opt.expandtab = true
-opt.breakindent = true
 
-opt.relativenumber = true
+----------------------------------------------------------------------
+-- Fancy icons (for telescope, nvim-tree, and lualine)
+--
+local devicons = require 'nvim-web-devicons'
+devicons.setup({ default = true })
 
-opt.mouse = 'a'
 
-opt.completeopt = {'menu', 'menuone', 'noselect'}
+----------------------------------------------------------------------
+-- Fancy status line
+--
+local lualine = require('lualine')
+lualine.setup({ options = { theme = 'jellybeans' } })
