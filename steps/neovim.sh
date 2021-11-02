7227470f5ceb966993ddfc2aecc33cc106f0b9d2
Pete Yandell <pete@notahat.com>
2021-11-02 16:01:22 +1100

    Shallow clone the git submodules

--- steps/neovim.sh
+++ steps/neovim.sh
@@ -2,7 +2,7 @@ mkdir -p ~/.config/nvim
 link_file ~/.dotfiles/nvim/init.lua ~/.config/nvim/init.lua
 
 # Our neovim plugins are in submodules, so make sure we've got them.
-git submodule update --init
+git submodule update --init --depth 1
 
 mkdir -p ~/.local/share/nvim/site/pack
 link_file ~/.dotfiles/nvim/plugins ~/.local/share/nvim/site/pack/plugins
