09fa3f768b633bc54855e90240dcb52e9017d5de
Pete Yandell <pete@notahat.com>
2021-11-03 11:27:12 +1100

    Install vim-vsnip
    Turns out nvim-cmp needs a snippet plugin or it breaks.

--- nvim/init.lua
+++ nvim/init.lua
@@ -65,6 +65,11 @@ treesitter.setup({
 --
 local cmp = require 'cmp'
 cmp.setup({
+  snippet = {
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body)
+    end
+  },
   sources = cmp.config.sources({
     { name = 'nvim_lsp' }
   })
