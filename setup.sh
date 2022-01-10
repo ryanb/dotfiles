a2cdca41f6f39fbc42097531938ec2fb314bc46a
Steve Lounsbury <steve.lounsbury@shopify.com>
2022-01-10 14:17:11 -0500

    Temporarily disable setup script.

--- setup.sh
+++ setup.sh
@@ -1,20 +1,20 @@
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-curl -fLo ~/.vim/colors/deus.vim --create-dirs https://raw.githubusercontent.com/ajmwagar/vim-deus/master/colors/deus.vim
+# curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# curl -fLo ~/.vim/colors/deus.vim --create-dirs https://raw.githubusercontent.com/ajmwagar/vim-deus/master/colors/deus.vim
 
-ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
-ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/dircolors.256dark ~/.dircolors.256dark
+# ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
+# ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
+# ln -sf ~/dotfiles/dircolors.256dark ~/.dircolors.256dark
 
-if ! command -v rg &> /dev/null; then
-  sudo apt-get install -y ripgrep
-fi
+# if ! command -v rg &> /dev/null; then
+#  sudo apt-get install -y ripgrep
+# fi
 
-if ! command -v fzf &> /dev/null; then
-  sudo apt-get install -y fzf
-fi
+# if ! command -v fzf &> /dev/null; then
+#  sudo apt-get install -y fzf
+# fi
 
-vim +PlugInstall +qall
+# vim +PlugInstall +qall
 
-source ~/.zshrc
+# source ~/.zshrc
