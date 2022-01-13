0ff25cc1afe0de12018b5b061c8c85866f9294b3
Steve Lounsbury <steve.lounsbury@shopify.com>
2022-01-13 13:32:50 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,20 +1,20 @@
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-# curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-# curl -fLo ~/.vim/colors/deus.vim --create-dirs https://raw.githubusercontent.com/ajmwagar/vim-deus/master/colors/deus.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/colors/deus.vim --create-dirs https://raw.githubusercontent.com/ajmwagar/vim-deus/master/colors/deus.vim
 
-# ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
-# ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
+ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
 # ln -sf ~/dotfiles/dircolors.256dark ~/.dircolors.256dark
 
-# if ! command -v rg &> /dev/null; then
-#  sudo apt-get install -y ripgrep
-# fi
+if ! command -v rg &> /dev/null; then
+  sudo apt-get install -o DPkg::Lock::Timeout=600 -y ripgrep
+fi
 
-# if ! command -v fzf &> /dev/null; then
-#  sudo apt-get install -y fzf
-# fi
+if ! command -v fzf &> /dev/null; then
+  sudo apt-get install -o DPkg::Lock::Timeout=600 -y fzf
+fi
 
-# vim +PlugInstall +qall
+vim.basic +PlugInstall +qall
 
 # source ~/.zshrc
