fcefc72c0b2f256a7bef2b405572a74ec23a1496
Steve Lounsbury <steve.lounsbury@shopify.com>
2022-01-18 21:55:06 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,10 @@
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 curl -fLo ~/.vim/colors/deus.vim --create-dirs https://raw.githubusercontent.com/ajmwagar/vim-deus/master/colors/deus.vim
 
 ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
 ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
-# ln -sf ~/dotfiles/dircolors.256dark ~/.dircolors.256dark
+ln -sf ~/dotfiles/zshrc ~/.zshrc
 
 if ! command -v rg &> /dev/null; then
   sudo apt-get install -o DPkg::Lock::Timeout=600 -y ripgrep
@@ -17,4 +16,4 @@ fi
 
 vim.basic +PlugInstall +qall
 
-# source ~/.zshrc
+source ~/.zshrc
