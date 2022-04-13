39b6bba7d75b864b9c0c7f5076df5f45b85e57a8
Pete Yandell <pete@notahat.com>
2022-04-13 22:06:11 +1000

    Move git submodule setup earlier
    Now that I'm using submodules for zsh plugins too, move submodule
    fetching earlier in the setup process.

--- steps/config.sh
+++ steps/config.sh
@@ -1,3 +1,6 @@
+# We use submodules for neovim and zsh plugins, so make sure we've got 'em.
+git submodule update --init --depth 1
+
 link_config_files finicky.js tool-versions zshrc zsh
 
 defaults write com.apple.dock show-recents -boolean FALSE
--- steps/neovim.sh
+++ steps/neovim.sh
@@ -1,7 +1,4 @@
 link_file ~/.dotfiles/nvim ~/.config/nvim
 
-# Our neovim plugins are in submodules, so make sure we've got them.
-git submodule update --init --depth 1
-
 # Rebuild the help index for all the plugins.
 nvim --headless -c "helptags ALL" -c "quitall"
