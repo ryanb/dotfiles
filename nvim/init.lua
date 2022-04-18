9bf931727bd33c2f5d2e32fee88f0a5cb2cb2aa8
Pete Yandell <pete@notahat.com>
2022-04-18 22:07:59 +1000

    Get less clever with lua in my neovim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,36 +1,33 @@
 ----------------------------------------------------------------------
 -- General options
 --
-local opt = vim.opt
-local cmd = vim.cmd
-
-opt.tabstop = 2
-opt.shiftwidth = 2
-opt.softtabstop = 2
-opt.expandtab = true
-opt.breakindent = true
-opt.linebreak = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+vim.opt.breakindent = true
+vim.opt.linebreak = true
 
 -- Use relative line numbering, but display the actual line
 -- number on the current line, and highlight it.
-opt.number = true
-opt.relativenumber = true
-opt.cursorline = true
-opt.cursorlineopt = 'number'
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.cursorlineopt = 'number'
 
-opt.scrolloff = 2
+vim.opt.scrolloff = 2
 
-opt.tildeop = true  -- make the ~ command behave like an operator
+vim.opt.tildeop = true  -- make the ~ command behave like an operator
 
-opt.showmatch = true  -- show matching brackets when typing
+vim.opt.showmatch = true  -- show matching brackets when typing
 
-opt.mouse = 'a'
+vim.opt.mouse = 'a'
 
 
 ----------------------------------------------------------------------
 -- Remove whitespace at the end of lines on save
 --
-cmd [[
+vim.cmd [[
 augroup vimrcCommands
   autocmd!
   autocmd BufWritePre * :%s/\s\+$//e
@@ -42,54 +39,54 @@ augroup END
 -- Random packages
 --
 function packadd(package)
-  cmd('packadd! '..package)
+  vim.cmd('packadd! '..package)
 end
 
-packadd 'plenary.nvim'
-packadd 'vim-commentary'
+packadd('plenary.nvim')
+packadd('vim-commentary')
 
 
 ----------------------------------------------------------------------
 -- Telescope
 --
-packadd 'telescope.nvim'
+packadd('telescope.nvim')
 -- We need this further down for some key bindings
-local telescope = require 'telescope.builtin'
+local telescope = require('telescope.builtin')
 
 
 ----------------------------------------------------------------------
 -- Colors
 --
-opt.termguicolors = true
-packadd 'lush.nvim'
-packadd 'jellybeans-nvim'
-cmd 'colorscheme jellybeans-nvim'
+vim.opt.termguicolors = true
+packadd('lush.nvim')
+packadd('jellybeans-nvim')
+vim.cmd('colorscheme jellybeans-nvim')
 
 
 ----------------------------------------------------------------------
 -- Fancy icons (for telescope, nvim-tree, and lualine)
 --
-packadd 'nvim-web-devicons'
-local devicons = require 'nvim-web-devicons'
+packadd('nvim-web-devicons')
+local devicons = require('nvim-web-devicons')
 devicons.setup({ default = true })
 
 
 ----------------------------------------------------------------------
 -- Fancy status line
 --
-packadd 'lualine.nvim'
-local lualine = require 'lualine'
+packadd('lualine.nvim')
+local lualine = require('lualine')
 lualine.setup({ options = { theme = 'auto' } })
 
-opt.showmode = false  -- lualine shows the mode for us
+vim.opt.showmode = false  -- lualine shows the mode for us
 
 
 ----------------------------------------------------------------------
 -- Syntax highlighting and text objects for functions
 --
-packadd 'nvim-treesitter'
-packadd 'nvim-treesitter-textobjects'
-local treesitter = require 'nvim-treesitter.configs'
+packadd('nvim-treesitter')
+packadd('nvim-treesitter-textobjects')
+local treesitter = require('nvim-treesitter.configs')
 treesitter.setup({
   ensure_installed = {
     'lua', 'javascript', 'typescript', 'tsx', 'css', 'scss', 'ruby'
@@ -119,25 +116,25 @@ treesitter.setup({
 ----------------------------------------------------------------------
 -- Auto-close brackets and tags and stuff
 --
-packadd 'nvim-autopairs'
-local autopairs = require 'nvim-autopairs'
+packadd('nvim-autopairs')
+local autopairs = require('nvim-autopairs')
 autopairs.setup{}
 
-packadd 'nvim-ts-autotag'
-local autotag = require 'nvim-ts-autotag'
+packadd('nvim-ts-autotag')
+local autotag = require('nvim-ts-autotag')
 autotag.setup()
 
 
 ----------------------------------------------------------------------
 -- Completion
 --
-packadd 'nvim-cmp'
-packadd 'cmp-nvim-lsp'
-packadd 'vim-vsnip' -- cmp doesn't work without a snippet plugin
+packadd('nvim-cmp')
+packadd('cmp-nvim-lsp')
+packadd('vim-vsnip') -- cmp doesn't work without a snippet plugin
 
-opt.completeopt = { 'menu', 'menuone', 'noselect' }
+vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
 
-local cmp = require 'cmp'
+local cmp = require('cmp')
 cmp.setup({
   snippet = {
     expand = function(args)
@@ -149,7 +146,7 @@ cmp.setup({
   }
 })
 
-local cmp_nvim_lsp = require 'cmp_nvim_lsp'
+local cmp_nvim_lsp = require('cmp_nvim_lsp')
 -- This is used to set up LSP further down.
 local cmp_capabilities = cmp_nvim_lsp.update_capabilities(
   vim.lsp.protocol.make_client_capabilities()
@@ -159,8 +156,8 @@ local cmp_capabilities = cmp_nvim_lsp.update_capabilities(
 ----------------------------------------------------------------------
 -- File navigation
 --
-packadd 'nvim-tree.lua'
-local nvim_tree = require 'nvim-tree'
+packadd('nvim-tree.lua')
+local nvim_tree = require('nvim-tree')
 nvim_tree.setup({
   open_on_setup = true,
   filters = { dotfiles = true },
@@ -172,60 +169,60 @@ nvim_tree.setup({
 ----------------------------------------------------------------------
 -- Automatic formatting
 --
-packadd 'neoformat'
+packadd('neoformat')
 vim.g.neoformat_try_node_exe = true
 
 
 ----------------------------------------------------------------------
 -- Git diffs in the sign column
 --
-packadd 'gitsigns.nvim'
-local gitsigns = require 'gitsigns'
+packadd('gitsigns.nvim')
+local gitsigns = require('gitsigns')
 gitsigns.setup()
 
 
 ----------------------------------------------------------------------
 -- Key bindings
 --
-local keymap = vim.keymap
+local bind = vim.keymap.set
 
 vim.g.mapleader = ','
 
 -- Shortcuts for navigation between windows
-keymap.set('n', '<c-h>', '<c-w>h')
-keymap.set('n', '<c-j>', '<c-w>j')
-keymap.set('n', '<c-k>', '<c-w>k')
-keymap.set('n', '<c-l>', '<c-w>l')
+bind('n', '<c-h>', '<c-w>h')
+bind('n', '<c-j>', '<c-w>j')
+bind('n', '<c-k>', '<c-w>k')
+bind('n', '<c-l>', '<c-w>l')
 
 -- Reselect the visual area when changing indenting in visual mode
-keymap.set('v', '<', '<gv')
-keymap.set('v', 'r', 'rgv')
+bind('v', '<', '<gv')
+bind('v', 'r', 'rgv')
 
 -- Leader mappings
-keymap.set('n', '<leader>b', function() telescope.buffers() end)
-keymap.set('n', '<leader>f', function() telescope.find_files() end)
-keymap.set('n', '<leader>p', '<cmd>Neoformat<cr>')
-keymap.set('n', '<leader>t', function() nvim_tree.toggle() end)
+bind('n', '<leader>b', function() telescope.buffers() end)
+bind('n', '<leader>f', function() telescope.find_files() end)
+bind('n', '<leader>p', '<cmd>Neoformat<cr>')
+bind('n', '<leader>t', function() nvim_tree.toggle() end)
 
 -- These bindings are set when a language server attaches to a buffer
 local function on_lsp_attach(client, buffer_number)
-  keymap.set('n', '<leader>ca', function() telescope.lsp_code_actions() end, { buffer = buffer_number })
-  keymap.set('v', '<leader>ca', function() telescope.lsp_range_code_actions() end, { buffer = buffer_number })
-  keymap.set('n', '<leader>cd', function() telescope.diagnostics() end, { buffer = buffer_number })
-  keymap.set('n', '<leader>cr', function() vim.lsp.buf.rename() end, { buffer = buffer_number })
-  keymap.set('n', '<leader>cs', function() telescope.lsp_document_symbols() end, { buffer = buffer_number })
+  bind('n', '<leader>ca', function() telescope.lsp_code_actions() end, { buffer = buffer_number })
+  bind('v', '<leader>ca', function() telescope.lsp_range_code_actions() end, { buffer = buffer_number })
+  bind('n', '<leader>cd', function() telescope.diagnostics() end, { buffer = buffer_number })
+  bind('n', '<leader>cr', function() vim.lsp.buf.rename() end, { buffer = buffer_number })
+  bind('n', '<leader>cs', function() telescope.lsp_document_symbols() end, { buffer = buffer_number })
 
-  keymap.set('n', 'K', function() vim.lsp.buf.hover() end, { buffer = buffer_number })
-  keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, { buffer = buffer_number })
-  keymap.set('n', 'gr', function() vim.lsp.buf.references() end, { buffer = buffer_number })
+  bind('n', 'K', function() vim.lsp.buf.hover() end, { buffer = buffer_number })
+  bind('n', 'gd', function() vim.lsp.buf.definition() end, { buffer = buffer_number })
+  bind('n', 'gr', function() vim.lsp.buf.references() end, { buffer = buffer_number })
 end
 
 
 ----------------------------------------------------------------------
 -- Language server
 --
-packadd 'nvim-lspconfig'
-local lsp = require 'lspconfig'
+packadd('nvim-lspconfig')
+local lsp = require('lspconfig')
 
 local lsp_opts = {
   on_attach = on_lsp_attach,
