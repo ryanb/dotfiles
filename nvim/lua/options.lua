1ff8e7bfed2134a4227015381e8390c6e6ca8758
Pete Yandell <pete@notahat.com>
2023-05-30 08:58:40 +1000

    Stop nvim's checkhealth complaining

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -17,5 +17,12 @@ return {
         vim.opt.relativenumber = true
         vim.opt.cursorline = true
         vim.opt.cursorlineopt = "number"
+
+        -- Stop checkhealth complaining about missing language providers.
+        -- I never use the language-specific interfaces anyway.
+        vim.g.loaded_python3_provider = 0
+        vim.g.loaded_ruby_provider = 0
+        vim.g.loaded_node_provider = 0
+        vim.g.loaded_perl_provider = 0
     end
 }
