9b6d4cdb51642a4a9ad47da439d3b1b099869ec3
Pete Yandell <pete@notahat.com>
2023-11-09 20:50:47 +1100

    Neovim config tinkering
    Tinkering with completion and test running.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,10 +1,22 @@
 local packadd = require("packadd")
+local bind = vim.keymap.set
 
-packadd("plenary.nvim") -- Some other packages need this.
+if vim.g.vscode then
+    -- Clean for now.
+else
+    packadd("plenary.nvim") -- Some other packages need this.
 
-require("options").configure()
-require("styling").configure()
-require("navigation").configure()
-require("plugins").configure()
+    require("options").configure()
+    require("styling").configure()
+    require("navigation").configure()
+    require("plugins").configure()
+    require("whitespace").remove_trailing_whitespace_on_save()
 
-require("whitespace").remove_trailing_whitespace_on_save()
+    packadd("neotest")
+    packadd("neotest-rspec")
+    require("neotest").setup({
+        adapters = {
+            require("neotest-rspec")
+        }
+    })
+end
--- nvim/lua/navigation.lua
+++ nvim/lua/navigation.lua
@@ -52,70 +52,73 @@ local function configure_key_bindings(telescope_builtin, nvim_tree_api)
     bind("n", "<leader>p", "<cmd>Neoformat<cr>")
     bind("n", "<leader>t", nvim_tree_api.tree.toggle)
 
-    -- These bindings are set when a language server attaches to a buffer.
-    local function on_lsp_attach(client, buffer_number)
-        bind("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = buffer_number })
-        bind("v", "<leader>ca", vim.lsp.buf.range_code_action, { buffer = buffer_number })
-        bind("n", "<leader>cd", telescope_builtin.diagnostics, { buffer = buffer_number })
-        bind("n", "<leader>cr", vim.lsp.buf.rename, { buffer = buffer_number })
-        bind("n", "<leader>cs", telescope_builtin.lsp_document_symbols, { buffer = buffer_number })
+    vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+        callback = function(args)
+            local buffer_number = args.buf
 
-        bind("n", "K", vim.lsp.buf.hover, { buffer = buffer_number })
-        bind("n", "gd", vim.lsp.buf.definition, { buffer = buffer_number })
-        bind("n", "gr", vim.lsp.buf.references, { buffer = buffer_number })
-    end
+            bind("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = buffer_number })
+            -- bind("v", "<leader>ca", vim.lsp.buf.range_code_action, { buffer = buffer_number })
+            bind("n", "<leader>cd", telescope_builtin.diagnostics, { buffer = buffer_number })
+            bind("n", "<leader>cr", vim.lsp.buf.rename, { buffer = buffer_number })
+            bind("n", "<leader>cs", telescope_builtin.lsp_document_symbols, { buffer = buffer_number })
 
-    return on_lsp_attach
+            bind("n", "K", vim.lsp.buf.hover, { buffer = buffer_number })
+            bind("n", "gd", vim.lsp.buf.definition, { buffer = buffer_number })
+            bind("n", "gr", vim.lsp.buf.references, { buffer = buffer_number })
+        end
+    })
 end
 
 local function configure_completion()
     packadd("nvim-cmp")     -- https://github.com/hrsh7th/nvim-cmp
-    packadd("cmp-nvim-lsp") -- https://github.com/sar/cmp-lsp.nvim
-    -- I don't use snippets, but cmp doesn't work without a snippet plugin, so:
     packadd("vim-vsnip")    -- https://github.com/hrsh7th/vim-vsnip
-
-    vim.opt.completeopt = { "menu", "menuone", "noselect" }
 
     local cmp = require("cmp")
     cmp.setup(
         {
+            sources = {
+              { name = "nvim_lsp" }
+            },
+            mapping = cmp.mapping.preset.insert({
+                ['<Tab>'] = cmp.mapping.select_next_item(),
+                ['<S-Tab'] = cmp.mapping.select_prev_item(),
+                ['<CR>'] = cmp.mapping.confirm({ select = true })
+            }),
+            -- I don't use snippets, but cmp doesn't work without a snippet plugin, so:
             snippet = {
                 expand = function(args)
                     vim.fn["vsnip#anonymous"](args.body)
                 end
             },
-            sources = {
-                { name = "nvim_lsp" }
-            }
         }
     )
-
-    local cmp_nvim_lsp = require("cmp_nvim_lsp")
-    return cmp_nvim_lsp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
 end
 
-local function configure_language_server(on_lsp_attach, cmp_capabilities)
+local function configure_language_server()
+    packadd("cmp-nvim-lsp") -- https://github.com/sar/cmp-lsp.nvim
+    local cmp_nvim_lsp = require("cmp_nvim_lsp")
     packadd("nvim-lspconfig") -- https://github.com/neovim/nvim-lspconfig
     local lsp = require("lspconfig")
 
     local lsp_opts = {
-        on_attach = on_lsp_attach,
-        capabilities = cmp_capabilities
+        capabilities = cmp_nvim_lsp.default_capabilities()
     }
 
-    lsp.tsserver.setup(lsp_opts)
-    lsp.solargraph.setup(lsp_opts)
-    lsp.lua_ls.setup(lsp_opts)
     -- For other language servers see:
     -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+    lsp.lua_ls.setup(lsp_opts)
+    lsp.sorbet.setup(lsp_opts)
+    -- lsp.tsserver.setup(lsp_opts)
 end
 
 return {
     configure = function()
         local telescope_builtin = configure_fuzzy_finder()
         local nvim_tree_api = configure_file_navigation()
-        local on_lsp_attach = configure_key_bindings(telescope_builtin, nvim_tree_api)
-        local cmp_capabilities = configure_completion()
-        configure_language_server(on_lsp_attach, cmp_capabilities)
+        configure_key_bindings(telescope_builtin, nvim_tree_api)
+
+        configure_completion()
+        configure_language_server()
     end
 }
