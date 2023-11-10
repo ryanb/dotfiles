d539617184185d1fe12eb3a82f8bf2d5cbe164ef
Pete Yandell <pete@notahat.com>
2023-11-11 10:24:42 +1100

    Tweak nvim-notify config

--- nvim/lua/plugins/nvim-notify.lua
+++ nvim/lua/plugins/nvim-notify.lua
@@ -1,7 +1,7 @@
 local function configure()
     vim.cmd.packadd({"nvim-notify", bang = true}) -- https://github.com/rcarriga/nvim-notify
     local notify = require("notify")
-    notify.setup({})
+    notify.setup({background_colour = "#000000", stages = "fade", top_down = false})
 
     -- Use it for all the notifications.
     vim.notify = require("notify")
