241cd4c83a21cbc0080a1224e6aa3158ef8f1e8d
Pete Yandell <pete@notahat.com>
2023-11-14 12:05:37 +1100

    Didn't need as much lualine config

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -9,12 +9,7 @@ local function configure()
     lualine.setup({
         options = { theme = "auto" },
         sections = {
-            lualine_a = { "mode" },
-            lualine_b = { "branch", "diff", "diagnostics" },
             lualine_c = { { "filename", path = 1, on_click = copy_relative_path } },
-            lualine_x = { "encoding", "fileformat", "filetype" },
-            lualine_y = { "progress" },
-            lualine_z = { "location" },
         },
     })
 
