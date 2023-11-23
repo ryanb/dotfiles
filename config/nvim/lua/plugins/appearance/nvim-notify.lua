8554e2436a9bce569589fc773e54453ea84f9bc0
Pete Yandell <pete@notahat.com>
2023-11-23 19:53:44 +1100

    Fix nvim-notify config

--- config/nvim/lua/plugins/appearance/nvim-notify.lua
+++ config/nvim/lua/plugins/appearance/nvim-notify.lua
@@ -1,14 +1,14 @@
 -- https://github.com/rcarriga/nvim-notify
 
-local opts = {
-    background_colour = "#000000",
-    render = "wrapped-compact",
-    stages = "fade",
-    top_down = false,
-}
-
 local function config()
-    vim.notify = require("notify")
+    local notify = require("notify")
+    notify.setup({
+        background_colour = "#000000",
+        render = "wrapped-compact",
+        stages = "fade",
+        top_down = false,
+    })
+    vim.notify = notify
 end
 
-return { "rcarriga/nvim-notify", config = config, opts = opts }
+return { "rcarriga/nvim-notify", config = config }
