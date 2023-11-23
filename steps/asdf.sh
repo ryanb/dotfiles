dcad1377f5bfe89953fa8ae1b613e63c749c56a0
Pete Yandell <pete@notahat.com>
2023-11-24 10:20:24 +1100

    Bump work node version, and make reinstalling easier

--- steps/asdf.sh
+++ steps/asdf.sh
@@ -2,8 +2,8 @@ link_file environments/${DOTFILES_ENV}/tool-versions ~/.tool-versions
 link_file config/asdf/default-npm-packages ~/.default-npm-packages
 link_file config/asdf/default-gems ~/.default-gems
 
-asdf plugin add nodejs || true
-asdf install nodejs
+/opt/homebrew/bin/asdf plugin add nodejs || true
+/opt/homebrew/bin/asdf install nodejs
 
-asdf plugin add ruby || true
-asdf install ruby
+/opt/homebrew/bin/asdf plugin add ruby || true
+/opt/homebrew/bin/asdf install ruby
