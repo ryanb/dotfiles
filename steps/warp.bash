a022bf0b048c0f056d26289cda6cc80e3cecad8a
Pete Yandell <pete@notahat.com>
2023-11-25 23:04:42 +1100

    Fix Warp config

--- steps/warp.bash
+++ steps/warp.bash
@@ -2,7 +2,6 @@ defaults write dev.warp.Warp-Stable FontName -string '"BerkeleyMono Nerd Font"'
 defaults write dev.warp.Warp-Stable FontSize -string "14.0"
 defaults write dev.warp.Warp-Stable LineHeightRatio -string "1.4"
 defaults write dev.warp.Warp-Stable NewWindowsNumColumns -int 160
-defaults write dev.warp.Warp-Stable Autosuggestions -bool false
-defaults write dev.warp.Warp-Stable FontName -string '"Waterfall"'
+defaults write dev.warp.Warp-Stable Autosuggestions -string "false"
 
 echo Installed.
