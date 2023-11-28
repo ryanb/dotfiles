699e8abb79f6ee33d1851c57c2d9b40dd1717f23
Pete Yandell <pete@notahat.com>
2023-11-28 23:31:29 +1100

    Neovim colour tweaking

--- config/nvim/lua/plugins/appearance/jellybeans.lua
+++ config/nvim/lua/plugins/appearance/jellybeans.lua
@@ -1,21 +1,28 @@
 -- https://github.com/nanotech/jellybeans.vim
 
 local function config()
-    -- Use the terminal's background instead of black.
     vim.g.jellybeans_overrides = {
         background = { guibg = "none" },
         DiagnosticError = { guifg = "ff6666" },
+        GitSignsAdd = { guifg = "99bc80" },
+        GitSignsChange = { guifg = "68aee8" },
+        GitSignsDelete = { guifg = "e16d77" },
         NormalFloat = { guibg = "333333" },
         SignColumn = { guibg = "none" },
+        VertSplit = { guifg = "35373b", guibg = "35373b" },
     }
 
-    vim.cmd.colorscheme("jellybeans")
-
-    -- While we're fixing diagnostic colours, let's give them some nice icons too.
-    vim.cmd.sign("define", "DiagnosticSignError", "text=󰈸", "texthl=DiagnosticSignError")
-    vim.cmd.sign("define", "DiagnosticSignWarn", "text=", "texthl=DiagnosticSignWarn")
+    vim.cmd.sign("define", "DiagnosticSignError", "text=", "texthl=DiagnosticSignError")
+    vim.cmd.sign("define", "DiagnosticSignWarn", "text=󰀪", "texthl=DiagnosticSignWarn")
     vim.cmd.sign("define", "DiagnosticSignInfo", "text=i", "texthl=DiagnosticSignInfo")
     vim.cmd.sign("define", "DiagnosticSignHint", "text=󰍉", "texthl=DiagnosticSignHint")
+
+    vim.cmd.colorscheme("jellybeans")
 end
 
-return { "nanotech/jellybeans.vim", config = config, lazy = false, priority = 1000 }
+return {
+    "nanotech/jellybeans.vim",
+    config = config,
+    lazy = false,
+    priority = 1000,
+}
