5970421b34da0c653adb02103075f46170893776
Pete Yandell <pete@notahat.com>
2023-11-30 23:44:28 +1100

    Better treesitter key mappings
    "a" for function arguments, "f" for functions

--- config/nvim/lua/plugins/language/nvim-treesitter.lua
+++ config/nvim/lua/plugins/language/nvim-treesitter.lua
@@ -1,42 +1,69 @@
 -- https://github.com/nvim-treesitter/nvim-treesitter
 
+-- Ensure all these parsers are loaded.
+local parsers = {
+    "bash",
+    "css",
+    "graphql",
+    "html",
+    "javascript",
+    "json",
+    "jsonc",
+    "lua",
+    "ruby",
+    "scss",
+    "tsx",
+    "typescript",
+}
+
+-- Configuration for moving around by textobject.
+local textobjects_move = {
+    enable = true,
+    set_jumps = true,
+    goto_previous_start = {
+        ["[a"] = {
+            query = "@parameter.inner",
+            desc = "previous argument",
+        },
+        ["[f"] = {
+            query = "@function.outer",
+            desc = "previous function",
+        },
+    },
+    goto_next_start = {
+        ["]a"] = {
+            query = "@parameter.inner",
+            desc = "next argument",
+        },
+        ["]f"] = {
+            query = "@function.outer",
+            desc = "next function",
+        },
+    },
+}
+
+-- Configuration for selecting textobjects.
+local textobjects_select = {
+    enable = true,
+    lookahead = true,
+    keymaps = {
+        ["aa"] = { query = "@parameter.outer", desc = "an argument" },
+        ["ia"] = { query = "@parameter.inner", desc = "inner argument" },
+        ["af"] = { query = "@function.outer", desc = "a function" },
+        ["if"] = { query = "@function.inner", desc = "inner function" },
+    },
+}
+
 local function config()
     local configs = require("nvim-treesitter.configs")
 
     configs.setup({
-        ensure_installed = {
-            "bash",
-            "css",
-            "graphql",
-            "html",
-            "javascript",
-            "json",
-            "jsonc",
-            "lua",
-            "ruby",
-            "scss",
-            "tsx",
-            "typescript",
-        },
+        ensure_installed = parsers,
         highlight = { enable = true },
         incremental_selection = { enable = true },
         textobjects = {
-            select = {
-                enable = true,
-                lookahead = true,
-                keymaps = {
-                    ["af"] = "@function.outer",
-                    ["if"] = "@function.inner",
-                },
-            },
-            move = {
-                enable = true,
-                set_jumps = true,
-                goto_next_start = { ["]m"] = "@function.outer" },
-                goto_next_end = { ["]M"] = "@function.outer" },
-                goto_previous_start = { ["[m"] = "@function.outer" },
-                goto_previous_end = { ["[M"] = "@function.outer" },
-            },
+            move = textobjects_move,
+            select = textobjects_select,
         },
     })
 end
