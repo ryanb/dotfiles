d0a7d729be0cea3b76638158afd5c20ec3bcadb5
Pete Yandell <pete@notahat.com>
2024-02-12 11:28:48 +1100

    Simplify asdf setup
    We don't need to install any default gems or npm packages, so we can
    remove this for now.

--- steps/asdf.bash
+++ steps/asdf.bash
@@ -1,8 +1,6 @@
 # shellcheck shell=bash
 
 link_file "environments/$DOTFILES_ENV/tool-versions" ~/.tool-versions
-link_file config/asdf/default-npm-packages ~/.default-npm-packages
-link_file config/asdf/default-gems ~/.default-gems
 
 # Make sure we've got homebrew loaded, coz asdf is installed with it.
 eval "$(/opt/homebrew/bin/brew shellenv)"
@@ -11,7 +9,5 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 source "$HOMEBREW_PREFIX/opt/asdf/libexec/asdf.sh"
 
 asdf plugin add nodejs || true
-asdf install nodejs
-
 asdf plugin add ruby || true
-asdf install ruby
+asdf install
