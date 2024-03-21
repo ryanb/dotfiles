1213b6e8794836d4e5e7a80bf3e02a5ec13c5b73
Pete Yandell <pete@notahat.com>
2024-03-21 22:19:06 +1100

    Neaten neovim key mappings a little

--- config/nvim/lua/key_mappings.lua
+++ config/nvim/lua/key_mappings.lua
@@ -7,31 +7,33 @@ local actions = require("helpers.actions")
 local map = vim.keymap.set
 
 local function map_global_keys()
-    -- Leader + single key stuff:
+    -- Leader + upper case for Neo-tree
+    map("n", "<leader>B", actions.explore_buffers, { desc = "explore buffers" })
+    map("n", "<leader>E", actions.explore_files, { desc = "explore files" })
+    map("n", "<leader>F", actions.explore_current_file, { desc = "explore current file" })
+    map("n", "<leader>G", actions.explore_git_status, { desc = "explore git status" })
+
+    -- Leader + lower case for lots of other things
     map("n", "<leader><leader>", telescope_builtin.find_files, { desc = "find files" })
     map("n", "<leader>.", telescope_builtin.resume, { desc = "resume last find" })
     map("n", "<leader>/", telescope_builtin.live_grep, { desc = "find in project" })
     map("n", "<leader>b", telescope_builtin.buffers, { desc = "find buffers" })
-    map("n", "<leader>B", actions.explore_buffers, { desc = "explore buffers" })
     map("n", "<leader>d", vim.diagnostic.open_float, { desc = "show diagnostics under cursor" })
-    map("n", "<leader>E", actions.explore_files, { desc = "explore files" })
-    map("n", "<leader>F", actions.explore_current_file, { desc = "explore current file" })
-    map("n", "<leader>G", actions.explore_git_status, { desc = "explore git status" })
     map("n", "<leader>q", actions.write_all_and_quit, { desc = "write all files and quit" })
     map({ "n", "x" }, "<leader>r", refactoring.select_refactor, { desc = "refacor" })
     map("n", "<leader>w", actions.find_word_under_cursor, { desc = "find word under cursor" })
     map("n", "<leader>x", bufdelete.bufdelete, { desc = "close current buffer" })
 
-    -- Run tests:
+    -- Run tests
     map("n", "<leader>t.", actions.test_last, { desc = "repeat the last test run" })
     map("n", "<leader>tf", actions.test_file, { desc = "run tests in current file" })
     map("n", "<leader>tn", actions.test_nearest, { desc = "run nearest test" })
 
-    -- Restart things:
+    -- Restart things
     map("n", "<leader>ze", actions.restart_eslint, { desc = "restart eslint" })
     map("n", "<leader>zl", vim.cmd.LspRestart, { desc = "restart LSP" })
 
-    -- Move around in the buffer:
+    -- Move around in the buffer
     map("n", "[d", vim.diagnostic.goto_prev, { desc = "previous diagnostic in buffer" })
     map("n", "]d", vim.diagnostic.goto_next, { desc = "next diagnostic in buffer" })
     map("n", "[g", gitsigns.prev_hunk, { desc = "previous git hunk in buffer" })
@@ -39,13 +41,13 @@ local function map_global_keys()
     map("n", "[q", vim.cmd.cbefore, { desc = "previous quickfix error in buffer" })
     map("n", "]q", vim.cmd.cafter, { desc = "next quickfix error in buffer" })
 
-    -- Navigate between windows:
+    -- Navigate between windows
     map("n", "<c-h>", "<c-w>h")
     map("n", "<c-j>", "<c-w>j")
     map("n", "<c-k>", "<c-w>k")
     map("n", "<c-l>", "<c-w>l")
 
-    -- Reselect the visual area when changing indenting in visual mode:
+    -- Reselect the visual area when changing indenting in visual mode
     map("v", "<", "<gv")
     map("v", ">", ">gv")
 end
