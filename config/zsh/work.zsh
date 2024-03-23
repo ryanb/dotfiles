3e87f4c3bab0d124e39956f6b10d528375671f38
Pete Yandell <pete@notahat.com>
2024-03-23 17:12:35 +1100

    No need to add the Android SDK to the path twice

--- config/zsh/work.zsh
+++ config/zsh/work.zsh
@@ -1,3 +1,5 @@
+if [[ $DOTFILES_ENV != work ]] { return }
+
 if [[ -f $HOMEBREW_PREFIX/bin/mise ]] {
   if [[ -o interactive ]] {
     eval "$(mise activate zsh)"
@@ -8,20 +10,13 @@ if [[ -f $HOMEBREW_PREFIX/bin/mise ]] {
 
 if [[ ! -v $ANDROID_HOME ]] {
   export ANDROID_HOME=$HOME/Library/Android/sdk
+  export ANDROID_SDK_ROOT=$HOME/Library/Android/sdk
+
   export PATH=$ANDROID_HOME/emulator:$PATH
   export PATH=$ANDROID_HOME/tools:$PATH
   export PATH=$ANDROID_HOME/tools/bin:$PATH
   export PATH=$ANDROID_HOME/platform-tools:$PATH
   export PATH=$ANDROID_HOME/cmdline-tools/latest/bin:$PATH
-}
-
-if [[ ! -v $ANDROID_SDK_ROOT ]] {
-  export ANDROID_SDK_ROOT=$HOME/Library/Android/sdk
-  export PATH=$ANDROID_SDK_ROOT/emulator:$PATH
-  export PATH=$ANDROID_SDK_ROOT/tools:$PATH
-  export PATH=$ANDROID_SDK_ROOT/tools/bin:$PATH
-  export PATH=$ANDROID_SDK_ROOT/platform-tools:$PATH
-  export PATH=$ANDROID_SDK_ROOT/cmdline-tools/latest/bin:$PATH
 }
 
 if [[ -f ~/src/up/.zsh-up-completion/init.zsh ]] {
