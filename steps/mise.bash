1a0e2514982ae298849d58ce8d86cdf3f857c536
Pete Yandell <pete@notahat.com>
2024-05-05 17:34:09 +1000

    Fix mise config

--- steps/mise.bash
+++ steps/mise.bash
@@ -1,8 +1,8 @@
 # shellcheck shell=bash
 
 if [[ $DOTFILES_ENV == "home" ]]; then
-  mkdir -p .copnfig
-  link_file "environments/$DOTFILES_ENV/mise.toml" ~/.config/mise.toml
+  mkdir -p ~/.config/mise
+  link_file "environments/$DOTFILES_ENV/mise.toml" ~/.config/mise/config.toml
 
   # Make sure we've got homebrew loaded, coz mise is installed with it.
   eval "$(/opt/homebrew/bin/brew shellenv)"
