cc93490f463b4995b00e88ad0f3872f5dda71102
Pete Yandell <pete@notahat.com>
2024-05-29 20:47:26 +1000

    Work around nightfox bug with neotree

--- config/nvim/lua/plugins/appearance/nightfox.lua
+++ config/nvim/lua/plugins/appearance/nightfox.lua
@@ -3,20 +3,14 @@
 -- https://github.com/EdenEast/nightfox.nvim
 
 local opts = {
-    options = {
-        module_default = false,
-        modules = {
-            diagnostic = { enabled = true, background = true },
-            gitsigns = true,
-            lazy = true,
-            lsp_semantic_tokens = true,
-            native_lsp = { enabled = true, background = true },
-            neotree = true,
-            notify = true,
-            telescope = true,
-            treesitter = true,
-            whichkey = true,
+    groups = {
+        all = {
+            -- Work around this issue:
+            -- https://github.com/EdenEast/nightfox.nvim/issues/440
+            NeoTreeTitleBar = { fg = "#131a24", bg = "#71839b" },
         },
+    },
+    options = {
         transparent = true,
     },
 }
