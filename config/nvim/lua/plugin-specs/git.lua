bde8549a9e6f829be55844397a6a4c2db7c13fd1
Pete Yandell <pete@notahat.com>
2024-06-12 21:10:14 +1000

    Use plenary for the LazyGit window in Neovim

--- config/nvim/lua/plugin-specs/git.lua
+++ config/nvim/lua/plugin-specs/git.lua
@@ -11,7 +11,7 @@ local lazygit_spec = {
     "kdheepak/lazygit.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
-        vim.g.lazygit_floating_window_use_plenary = 0
+        vim.g.lazygit_floating_window_use_plenary = 1
     end,
 }
 
