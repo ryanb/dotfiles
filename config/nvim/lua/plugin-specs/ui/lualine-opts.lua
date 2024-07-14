bb6c166518ec326c686c052026bdda943e342aba
Pete Yandell <pete@notahat.com>
2024-07-14 23:58:51 +1000

    Show neovim position in file as a percentage

--- config/nvim/lua/plugin-specs/ui/lualine-opts.lua
+++ config/nvim/lua/plugin-specs/ui/lualine-opts.lua
@@ -4,12 +4,6 @@ local function copy_relative_path()
     vim.notify("Path copied: " .. path)
 end
 
-local function position()
-    local current = vim.fn.line(".")
-    local total = vim.fn.line("$")
-    return string.format("%d/%d", current, total)
-end
-
 return {
     extensions = { "lazy", "man", "mason", "quickfix" },
     options = { disabled_filetypes = { "neo-tree" } },
@@ -19,7 +13,7 @@ return {
         lualine_c = { { "filename", on_click = copy_relative_path, path = 1, shorting_target = 20 } },
         lualine_x = { "branch" },
         lualine_y = { "searchcount" },
-        lualine_z = { position },
+        lualine_z = { "%p%%/%L" },
     },
     inactive_sections = {
         lualine_a = {},
