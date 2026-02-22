e6177f5ac34112b05a4590facf60ba8e79e80f83
Ryan Bates <ryan@railscasts.com>
2026-02-21 22:26:03 -0800

    Disable nvim lazy plugin update checker

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -25,5 +25,5 @@ require("lazy").setup({
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = { enabled = false },
 })
