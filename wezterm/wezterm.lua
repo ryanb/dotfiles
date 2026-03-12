809ed4e3c3b33ba45a3f44c7640f8d465aefc844
Ryan Bates <ryan@railscasts.com>
2026-03-12 13:55:40 -0700

    Change color of tabs and title bar in WezTerm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,11 +2,13 @@ local wezterm = require 'wezterm'
 local workspace = require 'workspace'
 local config = wezterm.config_builder()
 
+local titlebar_bg = '#191919'
+
 config.color_scheme = 'Dracula+'
 config.window_decorations = 'RESIZE|INTEGRATED_BUTTONS'
 config.window_frame = {
-  active_titlebar_bg = '#191919',
-  inactive_titlebar_bg = '#191919',
+  active_titlebar_bg = titlebar_bg,
+  inactive_titlebar_bg = titlebar_bg,
 }
 
 config.initial_cols = 120
@@ -19,6 +21,28 @@ config.max_fps = 120
 
 config.colors = {
   cursor_bg = '#FFFFFF',
+  tab_bar = {
+    active_tab = {
+      bg_color = '#242424',
+      fg_color = '#ffffff',
+    },
+    inactive_tab = {
+      bg_color = titlebar_bg,
+      fg_color = '#888888',
+    },
+    inactive_tab_hover = {
+      bg_color = '#1e1e1e',
+      fg_color = '#ffffff',
+    },
+    new_tab = {
+      bg_color = titlebar_bg,
+      fg_color = '#888888',
+    },
+    new_tab_hover = {
+      bg_color = '#242424',
+      fg_color = '#ffffff',
+    },
+  },
 }
 
 config.keys = {
@@ -63,7 +87,7 @@ config.show_tab_index_in_tab_bar = false
 wezterm.on('update-status', function(window, pane)
   local workspace = window:active_workspace()
   window:set_right_status(wezterm.format {
-    { Background = { Color = '#191919' } },
+    { Background = { Color = titlebar_bg } },
     { Foreground = { Color = '#ffffff' } },
     { Attribute = { Intensity = 'Bold' } },
     { Text = workspace .. '      ' },
