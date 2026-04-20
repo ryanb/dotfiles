aafea1e9f7913f597d9b9900d65b9b149060a951
Ryan Bates <ryan@railscasts.com>
2026-04-20 13:06:17 -0700

    Unset upstream tracking after gbdr deletes remote branch

--- plugins/git.zsh
+++ plugins/git.zsh
@@ -9,7 +9,9 @@ alias gaap='git add -N . && git add -p'
 alias gb='git branch --sort=committerdate'
 alias gbd='git branch --delete'
 gbdr() {
-  git push origin --delete "${1:-$(git_current_branch)}"
+  local branch="${1:-$(git_current_branch)}"
+  git push origin --delete "$branch"
+  git branch --unset-upstream "$branch" 2>/dev/null
 }
 
 alias gco='git checkout'
